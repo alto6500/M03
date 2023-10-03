1f088d74165acf1e7d1f9dd98ac385b4d0881e81
Alvaro Torné Recuero <alto6500@proven.local>
2023-10-03 12:57:36 +0200

    ejercicio salarios

--- UF1-NF1(IF - BUCLES)/Exercici 18/src/exercici/pkg18/Exercici18.java
+++ UF1-NF1(IF - BUCLES)/Exercici 18/src/exercici/pkg18/Exercici18.java
@@ -16,24 +16,34 @@ public class Exercici18 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int horas, salario, salarioextra, salarionormal, salariototal, tarifanormal, tarifaextra;
+        int horas, salario, salarioextra, salarionormal, tarifanormal, tarifaextra;
         Scanner pantalla = new Scanner(System.in);
         System.out.println("¿Cuantas horas trabajas a la semana?");
         horas = pantalla.nextInt();
         if (horas <= 35){
-        salario = horas * 20;
-        System.out.println("Tu salario neto a la semana es " + salario);}
+            salario = horas * 20;
+            System.out.println("Tu salario es " + salario+"€" );}
         else if (horas > 35){
-        tarifanormal = 20;
-        tarifaextra = (int) (20 * 1.5);
-        salarioextra = (horas - 35)* tarifaextra ;
-        salarionormal =(35 * tarifanormal);
-        salariototal = salarionormal + salarioextra;
-        System.out.println("Tu salario neto a la semana es " + salariototal);}
-        
-        
-        
-                
+            tarifanormal = 20;
+            tarifaextra = (int) (20 * 1.5);
+            salarioextra = (horas - 35)* tarifaextra ;
+            salarionormal =(35 * tarifanormal);
+            salario = salarionormal + salarioextra;
+         
+        if (salario>500){
+            double salario2, salario3, salario4, salariolimpio;
+            salario2 = salario - 500;
+            salario3 = (salario2 * 25)/100;
+            salario4 = salario2 - salario3;
+            salariolimpio = salario4 + 500;
+            System.out.println("Tu salario bruto a la semana es " + salario+"€");
+            System.out.println("Los impuestos son de " + salario3+"€");
+            System.out.println("Tu salario neto a la semana es " + salariolimpio+"€");}
+        else
+            System.out.println("Tu salario neto a la semana es " + salario+"€");
+        }
     }
-    
 }
+    
+
+
